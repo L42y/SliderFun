ea368837b887c58561b15e6bb254669ecc1e2bc1
Biao Xie <biao@polarr.co>
2021-02-03 14:24:46 +0800

    Initial Commit

---
+++ SliderFun/SliderFunApp.swift
@@ -0,0 +1,17 @@
+//
+//  SliderFunApp.swift
+//  SliderFun
+//
+//  Created by Biao Xie on 2/3/21.
+//
+
+import SwiftUI
+
+@main
+struct SliderFunApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
